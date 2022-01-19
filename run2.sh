bb49b158b69d300b8453274ca702bd01fe59361e
Diana Malyk <d.malyk@mobidev.biz>
2022-01-19 19:50:02 +0200

    del run

--- run2.sh
+++
@@ -1,5 +0,0 @@
-python3 main.py --run_id ex_1 --pooling_type max_pool2d --epochs 15
-
-python3 main.py --run_id ex_2_w+p --pooling_type generalized_lehmer_pool --alpha 1.5 --beta 0.1  --epochs 5
-
-python3 main.py --run_id ex_4_w+p --pooling_type generalized_power_mean_pool --gamma 1.5 --delta 0.2 --epochs 5
