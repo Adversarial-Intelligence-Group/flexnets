a2880610d984be5349c6761828f0a64ef30a1a08
Vladyslav BRANYTSKYI <v.branitsky@outlook.com>
2022-01-21 16:52:48 +0200

    Added fixes

--- flexnets/models/mlp.py
+++ flexnets/models/mlp.py
@@ -6,26 +6,29 @@ import torchmetrics
 from flexnets.nn.gln import GeneralizedLehmerLayer
 from flexnets.training.utils import clip_poolings
 
+
 class MLP(pl.LightningModule):
-    def __init__(self, in_features):
+    def __init__(self, in_features, out_features):
         super().__init__()
         self.accuracy = torchmetrics.Accuracy()
         self.layers = nn.Sequential(
-            # nn.Linear(in_features, 100),
-            # nn.Linear(100, 100),
-            # nn.Linear(100, 3)
-            GeneralizedLehmerLayer(in_features, 100),
-            nn.ReLU(),
-            GeneralizedLehmerLayer(100, 100),
-            nn.ReLU(),
-            GeneralizedLehmerLayer(100, 3)
+            # nn.Linear(in_features, 250),
+            # nn.ReLU(),
+            # nn.Linear(250, 100),
+            # nn.ReLU(),
+            # nn.Linear(100, out_features)
+
+            GeneralizedLehmerLayer(in_features, 250),
+            # nn.ReLU(),
+            GeneralizedLehmerLayer(250, 100),
+            # nn.ReLU(),
+            GeneralizedLehmerLayer(100, out_features)
         )
         # self.softmax = nn.Softmax(dim=1)
         self.ce = nn.CrossEntropyLoss()
 
     def forward(self, x):
         x = self.layers(x)
-        # return self.softmax(x)
         return x
 
     def training_step(self, batch, batch_idx):
@@ -37,7 +40,12 @@ class MLP(pl.LightningModule):
 
         acc = self.accuracy(y_hat, y)
         self.log('train_accuracy', acc, on_step=True, on_epoch=False)
+
+        for name, p in self.layers.named_parameters():
+            if ("alpha" in name) or ("beta" in name):
+                self.log(name, p.data)
         
+        clip_poolings(self.layers)
         return { 'loss': loss, 'accuracy': acc }
     
     def validation_step(self, batch, batch_idx):
@@ -50,6 +58,7 @@ class MLP(pl.LightningModule):
         acc = self.accuracy(y_hat, y)
         self.log('val_accuracy', acc, on_step=True, on_epoch=False)
 
+        clip_poolings(self.layers)
         return { 'loss': loss, 'accuracy': acc }
 
     def configure_optimizers(self):
