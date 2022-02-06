cf09f5e6b7aabdd03e54957b211d615245fc350f
Diana Malyk <d.malyk@mobidev.biz>
2022-02-06 11:40:53 +0200

    add structure

--- flexnets/nn/convolution.py
+++ flexnets/nn/convolution.py
@@ -103,20 +103,106 @@ class SoftLehmerConv2d(torch.nn.modules.conv._ConvNd):
         return self._conv_forward(input, self.weight, self.bias)
 
 
-class GeneralizedLehmerConvolution:
+class GeneralizedLehmerConvolution(torch.nn.modules.conv._ConvNd):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: Union[str, _size_2_t] = 0,
+        dilation: _size_2_t = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = 'zeros',  # TODO: refine this type
+        device=None,
+        dtype=None
+    ) -> None:
+        # factory_kwargs = {'device': device, 'dtype': dtype}
+        kernel_size_ = _pair(kernel_size)
+        stride_ = _pair(stride)
+        padding_ = padding if isinstance(padding, str) else _pair(padding)
+        dilation_ = _pair(dilation)
+        super(SoftLehmerConv2d, self).__init__(
+            in_channels, out_channels, kernel_size_, stride_, padding_, dilation_,
+            False, _pair(0), groups, bias, padding_mode)
+        # TODO #FIXME
+        # self.k = nn.Parameter(torch.tensor(0.), requires_grad=False)
 
-    def forward(self):
-        pass
-
-class GeneralizedPowerConvolution:
-
-    def __init__(self) -> None:
-        pass
-
-    def forward(self):
-        pass
+    def forward(self, input: Tensor) -> Tensor:
+        return self._conv_forward(input, self.weight, self.bias)
 
     
+    def _conv_forward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
+        if self.padding_mode != 'zeros':
+            x = F.pad(input, self._reversed_padding_repeated_twice,
+                      mode=self.padding_mode)
+        else:
+            x = F.pad(input, self._reversed_padding_repeated_twice,
+                      "constant", 0)
+
+        output_shape = ((x.shape[2] - self.kernel_size[0])//self.stride[0] + 1,
+                        (x.shape[3] - self.kernel_size[1])//self.stride[1] + 1)
+        # TODO #FIXME
+        # x = F.unfold(x, self.kernel_size, self.dilation,
+        #              self.padding, self.stride)  # torch.Size([32, 25, 576])
+        # x = torch.log((torch.exp(x*(self.k+1)).transpose(1, 2).matmul(weight.reshape(weight.size(
+        #     0), -1).t()).transpose(1, 2)/torch.exp(x*self.k).transpose(1, 2).matmul(weight.reshape(weight.size(
+        #         0), -1).t()).transpose(1, 2)))  # torch.Size([32, 10, 576]) # FIXME I think that *n is redundant
+        # torch.Size([32, 10, 24, 24])
+        x = x.reshape(input.shape[0], self.out_channels, *output_shape)
+        return x
+
+
+class GeneralizedPowerConvolution(torch.nn.modules.conv._ConvNd):
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: Union[str, _size_2_t] = 0,
+        dilation: _size_2_t = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = 'zeros',  # TODO: refine this type
+        device=None,
+        dtype=None
+    ) -> None:
+        # factory_kwargs = {'device': device, 'dtype': dtype}
+        kernel_size_ = _pair(kernel_size)
+        stride_ = _pair(stride)
+        padding_ = padding if isinstance(padding, str) else _pair(padding)
+        dilation_ = _pair(dilation)
+        super(SoftLehmerConv2d, self).__init__(
+            in_channels, out_channels, kernel_size_, stride_, padding_, dilation_,
+            False, _pair(0), groups, bias, padding_mode)
+        # TODO #FIXME
+        # self.k = nn.Parameter(torch.tensor(0.), requires_grad=False)
+
+    def forward(self, input: Tensor) -> Tensor:
+        return self._conv_forward(input, self.weight, self.bias)
+
+    
+    def _conv_forward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
+        if self.padding_mode != 'zeros':
+            x = F.pad(input, self._reversed_padding_repeated_twice,
+                      mode=self.padding_mode)
+        else:
+            x = F.pad(input, self._reversed_padding_repeated_twice,
+                      "constant", 0)
+
+        output_shape = ((x.shape[2] - self.kernel_size[0])//self.stride[0] + 1,
+                        (x.shape[3] - self.kernel_size[1])//self.stride[1] + 1)
+        # TODO #FIXME
+        # x = F.unfold(x, self.kernel_size, self.dilation,
+        #              self.padding, self.stride)  # torch.Size([32, 25, 576])
+        # x = torch.log((torch.exp(x*(self.k+1)).transpose(1, 2).matmul(weight.reshape(weight.size(
+        #     0), -1).t()).transpose(1, 2)/torch.exp(x*self.k).transpose(1, 2).matmul(weight.reshape(weight.size(
+        #         0), -1).t()).transpose(1, 2)))  # torch.Size([32, 10, 576]) # FIXME I think that *n is redundant
+        # torch.Size([32, 10, 24, 24])
+        x = x.reshape(input.shape[0], self.out_channels, *output_shape)
+        return x
+
