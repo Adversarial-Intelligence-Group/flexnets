2e0e8ee1f4965a75b2c57779f3d4f43d66d2cad5
Diana Malyk <d.malyk@mobidev.biz>
2022-05-30 23:55:13 +0300

    add bias

--- flexnets/nn/convolution.py
+++ flexnets/nn/convolution.py
@@ -6,10 +6,9 @@ import torch.nn.functional as F
 from torch.nn.modules.utils import _pair
 from torch.nn.common_types import _size_2_t
 from typing import Optional, Union
-
+import math
 
 class GeneralizedLehmerConvolution(torch.nn.modules.conv._ConvNd):
-
     def __init__(
         self,
         in_channels: int,
@@ -31,13 +30,12 @@ class GeneralizedLehmerConvolution(torch.nn.modules.conv._ConvNd):
             in_channels, out_channels, kernel_size_, stride_, padding_, dilation_,
             False, _pair(0), groups, bias, padding_mode)
         # TODO 
-        self.alpha = nn.Parameter(torch.tensor(2.5), requires_grad=True)
+        self.alpha = nn.Parameter(torch.tensor(1.5), requires_grad=True)
         self.beta = nn.Parameter(torch.tensor(1.3), requires_grad=True)
 
     def forward(self, input: Tensor) -> Tensor:
         return self._conv_forward(input, self.weight, self.bias)
 
-    
     def _conv_forward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
         if self.padding_mode != 'zeros':
             x = F.pad(input, self._reversed_padding_repeated_twice,
@@ -50,13 +48,16 @@ class GeneralizedLehmerConvolution(torch.nn.modules.conv._ConvNd):
                         (x.shape[3] - self.kernel_size[1])//self.stride[1] + 1)
         # x: torch.Size([64, 3, 34, 34]) 3 -> 32 ch
         x = F.unfold(input=x, kernel_size=self.kernel_size, dilation=self.dilation, stride=self.stride)  # torch.Size([64, 27, 1024])
-        n = self.kernel_size[0]*self.kernel_size[1]
-        multiplier = n/torch.log(self.alpha)
-        dotprod = x.transpose(1, 2).matmul(weight.reshape(weight.shape[0], -1).t()).transpose(1, 2) #torch.Size([64, 32, 1024])
-        numerator = torch.pow(self.alpha, (self.beta+1)*dotprod)
-        denominator = torch.pow(self.alpha, self.beta*dotprod)
-        x = multiplier*torch.log(numerator/denominator)  #torch.Size([64, 32, 1024])
-        x = x.reshape(input.shape[0], self.out_channels, *output_shape) # torch.Size([64, 32, 32, 32])
+        # n = self.kernel_size[0]*self.kernel_size[1]
+        # multiplier = n/torch.log(self.alpha)
+        dotprod = x.transpose(1, 2).matmul(weight.reshape(weight.shape[0], -1).t()).transpose(1, 2)  #torch.Size([64, 32, 1024])
+        # numerator = torch.pow(self.alpha, (self.beta+1)*dotprod)
+        # denominator = torch.pow(self.alpha, self.beta*dotprod)
+        # x = multiplier*torch.log(numerator/denominator)  #torch.Size([64, 32, 1024])
+        # x = x.reshape(input.shape[0], self.out_channels, *output_shape) # torch.Size([64, 32, 32, 32])
+        x = F.fold(dotprod, output_shape, (1, 1))
+        if bias is not None:
+            x += bias.reshape(1, bias.shape[0], 1, 1)
         return x
 
 
@@ -108,5 +109,7 @@ class GeneralizedPowerConvolution(torch.nn.modules.conv._ConvNd):
         dotprod_yd = torch.log(torch.pow(self.gamma, self.delta*dotprod)) - torch.log(n)
         x = multiplier*dotprod_yd #torch.Size([64, 32, 1024])
         x = x.reshape(input.shape[0], self.out_channels, *output_shape) # torch.Size([64, 32, 32, 32])
+        if bias is not None:
+            x += bias.reshape(1, bias.shape[0], 1, 1)
         return x
 
